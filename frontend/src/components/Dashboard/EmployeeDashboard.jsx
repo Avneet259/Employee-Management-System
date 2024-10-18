301f7836d2f2449380b8ee74aa9afbfe538b0b7d
AVNEET KAUR <avibal1325@gmail.com>
2024-10-18 15:09:53 +0530

    dummy EmployeeDashboard added

--- frontend/src/components/Dashboard/EmployeeDashboard.jsx
+++ frontend/src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header from '../../others/Header'
+import TaskListNumbers from '../../others/TaskListNumbers'
+import TaskList from '../Tasklist/TaskList'
 
 const EmployeeDashboard = () => {
   return (
-    <div>
-      EmployeeDashboard
+    <div className='p-10 h-screen bg-[#1C1C1C]'>
+      <Header />
+      <TaskListNumbers />
+      <TaskList />
     </div>
   )
 }
---
+++ frontend/src/components/Tasklist/TaskList.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div id='tasklist' className='h-[55%] w-full flex items-center justify-start gap-5 flex-nowrap py-5 mt-10 overflow-x-auto'>
+      <div className='h-full w-[350px] bg-red-400 rounded-xl flex-shrink-0 p-5'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>25 sept 2024</h4>
+        </div>
+        <h2 className='text-2xl font-semibold mt-5'>Make a project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus mollitia dignissimos quod quo qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illum similique totam harum quod optio!</p>
+      </div>
+      <div className='h-full w-[350px] bg-green-400 rounded-xl flex-shrink-0 p-5'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>25 sept 2024</h4>
+        </div>
+        <h2 className='text-2xl font-semibold mt-5'>Make a project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus mollitia dignissimos quod quo qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illum similique totam harum quod optio!</p>
+      </div>
+      <div className='h-full w-[350px] bg-blue-400 rounded-xl flex-shrink-0 p-5'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>25 sept 2024</h4>
+        </div>
+        <h2 className='text-2xl font-semibold mt-5'>Make a project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus mollitia dignissimos quod quo qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illum similique totam harum quod optio!</p>
+      </div>
+      <div className='h-full w-[350px] bg-sky-400 rounded-xl flex-shrink-0 p-5'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>25 sept 2024</h4>
+        </div>
+        <h2 className='text-2xl font-semibold mt-5'>Make a project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus mollitia dignissimos quod quo qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illum similique totam harum quod optio!</p>
+      </div>
+      <div className='h-full w-[350px] bg-yellow-400 rounded-xl flex-shrink-0 p-5'>
+        <div className='flex justify-between items-center'>
+            <h3 className='bg-red-600 text-sm px-3 py-1 rounded'>High</h3>
+            <h4 className='text-sm'>25 sept 2024</h4>
+        </div>
+        <h2 className='text-2xl font-semibold mt-5'>Make a project</h2>
+        <p className='text-sm mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta voluptatibus mollitia dignissimos quod quo qui? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, illum similique totam harum quod optio!</p>
+      </div>
+    </div>
+  )
+}
+
+export default TaskList
