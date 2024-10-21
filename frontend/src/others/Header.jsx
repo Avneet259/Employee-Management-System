ede8d4844d2ca45841bc46292845f0dd92e384b2
AVNEET KAUR <avibal1325@gmail.com>
2024-10-21 20:12:34 +0530

    logout feature is added

--- frontend/src/others/Header.jsx
+++ frontend/src/others/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Header = () => {
+
+  const logOut = () => {
+    localStorage.setItem('loggedInUser', '')
+    window.location.reload()
+  }
+
   return (
     <div className='flex items-end justify-between'>
       <div className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>Avneet 👋🏻</span></div>
-      <button className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
+      <button onClick={logOut} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
 }
