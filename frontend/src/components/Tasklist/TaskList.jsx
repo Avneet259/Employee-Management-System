9f25f255a24b7cc2a9ef61f03ca6acd692d974d5
AVNEET KAUR <avibal1325@gmail.com>
2024-10-22 13:36:45 +0530

    data is passed in tasks and localStorage file is modified

--- frontend/src/components/Tasklist/TaskList.jsx
+++ frontend/src/components/Tasklist/TaskList.jsx
@@ -9,16 +9,16 @@ const TaskList = ({data}) => {
     <div id='tasklist' className='h-[55%] w-full flex items-center justify-start gap-5 flex-nowrap py-5 mt-10 overflow-x-auto'>
       {data.tasks.map((task, index) => {
         if (task.active) {
-          return <AcceptTask key={index}/>
+          return <AcceptTask key={index} data={task}/>
         }
         if (task.newTask) {
-          return <NewTask key={index}/>
+          return <NewTask key={index} data={task}/>
         }
         if (task.completedTask) {
-          return <CompleteTask key={index}/>
+          return <CompleteTask key={index} data={task}/>
         }
         if (task.failedTask) {
-          return <FailedTask key={index}/>
+          return <FailedTask key={index} data={task}/>
         }
       })}
     </div>
