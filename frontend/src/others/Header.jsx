7d7183b13b82e44749c71cc24000fc65ba9bf47a
AVNEET KAUR <avibal1325@gmail.com>
2024-10-23 23:42:15 +0530

    setting data to localStorage feature added with some file modifications

--- frontend/src/others/Header.jsx
+++ frontend/src/others/Header.jsx
@@ -10,7 +10,7 @@ const Header = ({setUser}) => {
 
   return (
     <div className='flex items-end justify-between'>
-      <div className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>Avneet 👋🏻</span></div>
+      <div className='text-2xl font-medium'>Hello <br /> <span className='text-3xl font-semibold'>UserName 👋🏻</span></div>
       <button onClick={logOut} className='bg-red-600 text-lg font-medium text-white px-5 py-2 rounded-sm'>Log Out</button>
     </div>
   )
